c1c5ba9460d69bb12a8de52fa3bcf22234b8c2cf
Jean Tubay <jean.tubay.92@gmail.com>
2020-02-10 13:53:43 -0500

    added scroll spy functionality

---
+++ javaScript/scrollSpy.js
@@ -0,0 +1,19 @@
+(function(){
+    const navLinks = $('nav ul li a');
+    const navH = $('nav').height();
+    const section = $('article')
+    const documentEl = $(document);
+
+    documentEl.on('scroll', function(){
+        const currentScrollPos = documentEl.scrollTop();
+        section.each(function(){
+            const self = $(this);
+            if(self.offset().top < (currentScrollPos + navH) && (currentScrollPos + navH) < (self.offset().top + self.outerHeight())){
+                const targetclass = '.' + self.attr('class') + '-marker'
+                navLinks.removeClass('active');
+                $(targetclass).addClass('active')
+            }
+        })
+    })
+
+})();
