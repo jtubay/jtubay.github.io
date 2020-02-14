d2b97e5c330e45c580994d9b7f74b4c1060af1ac
Jean Tubay <jean.tubay.92@gmail.com>
2020-02-13 21:41:55 -0500

    adding styling

--- javaScript/typeWriter.js
+++ javaScript/typeWriter.js
@@ -1,4 +1,4 @@
-const texts = ['HOOT HOOT MUTHERFACKER!'];
+const texts = ['Hello! I am Jean Tubay, a full stack developer. I work with JS (with many frame works), mongoDB and MYSQL. Look at my work below'];
 let count = 0;
 let index = 0;
 let currentText = '';
@@ -17,6 +17,6 @@ let letter = '';
         count++
         index = 0;
     }
-    setTimeout(type, 100)
+    setTimeout(type, 200)
 
 }())
