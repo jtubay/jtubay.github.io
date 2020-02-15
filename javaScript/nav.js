100fe153ce856fed5b1ca3b4b2650c97716a934e
Jean Tubay <jean.tubay.92@gmail.com>
2020-02-15 00:42:10 -0500

    finish navbar reactive

--- javaScript/nav.js
+++ javaScript/nav.js
@@ -16,10 +16,24 @@ window.onscroll = () => {
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
-
+    const navLinks = document.querySelectorAll('.nav-links li')
     burger.addEventListener('click', ()=>{
+        //toggle
         nav.classList.toggle('navActive')
-    })
+        //active links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            } else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`
+            }
+            
+            
+        });
+        //burger animation
+        burger.classList.toggle('toggle')
+
+    });
 }
 const app = ()=> {
     navSlide()
